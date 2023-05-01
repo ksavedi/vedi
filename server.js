843c6fb2286456919b3f854b65e05c1bb70f04ae
Junee <juneekim7@gmail.com>
2023-05-01 20:27:08 +0900

    basic

---
+++ server.js
@@ -0,0 +1,17 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+import express from 'express'
+
+const app = express()
+app.use(express.static(__dirname))
+app.use(express.static(__dirname + '/client'))
+app.use(express.static(__dirname + '/data'))
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/client/main/index.html');
+})
+app.listen(80, () => {
+    console.log('server started!')
+})
