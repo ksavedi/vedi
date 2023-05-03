e8a4a1594eb41d1146e4db2899055da5d94de6fc
Junee <juneekim7@gmail.com>
2023-05-03 22:24:11 +0900

    socket

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,27 +1,18 @@
+import type { Auth } from '../../interface'
 const socket = new WebSocket('ws://localhost:3000')
 
-type auth = { token: string, id: string, pw: string }
-
-const data: { auth: auth, [key: string]: object } = {
-    auth: {
-        token: window.localStorage['token'],
-        id: window.localStorage['id'],
-        pw: window.localStorage['pw']
-    }
-}
-
-let msg: { type: string, content: object }
-socket.onmessage = (event) => {
-    msg = JSON.parse(event.data)
-    data[msg.type] = msg.content
+const auth: Auth = {
+    token: window.localStorage['token'],
+    id: window.localStorage['id'],
+    pw: window.localStorage['pw']
 }
 
 function send(type: string, content: object) {
     socket.send(JSON.stringify({
         type,
         content,
-        auth: data.auth
+        auth
     }))
 }
 
-export { data, send }
+export { auth, send }
