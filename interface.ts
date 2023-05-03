1c6380b57928f60711a9c3bd06c8d72588b87c1b
bennyk0406 <kbenny0406@gmail.com>
2023-05-03 22:31:45 +0900

    a little change

--- interface.ts
+++ interface.ts
@@ -3,7 +3,7 @@ type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 type Id = `${Batch}-${0 | 1 | 2}${Digit}${Digit}`
 
 interface Auth {
-    token: null | string;
+    token: string | null;
     id?: Id;
     pw?: string;
 }
@@ -14,7 +14,7 @@ interface Msg {
 }
 
 interface ClientMsg extends Msg {
-    auth: Auth
+    auth: Auth;
 }
 
-export { Id, Auth, Msg, ClientMsg }
+export type { Auth, Id, Msg, ClientMsg }
