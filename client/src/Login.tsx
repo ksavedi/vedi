250720f9d2feefbc3adee19d35b5f1c7980edd4c
bennyk0406 <kbenny0406@gmail.com>
2023-05-04 01:27:04 +0900

    change frontend

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -1,52 +1,94 @@
+/** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import { useState } from 'react'
 import { auth, send } from './socket'
 import { Id } from '../../interface'
 
-const requestLogin = (id: Id, pw: string) => {
+const requestLogin = (id: string, pw: string) => {
+    if (!/^(19|20|21|22|23|24)-\d{3}$/.test(id)) {
+        window.alert("형식에 맞지 않는 아이디입니다.")
+        return
+    }
     send({
         type: 'login',
-        content: { id, pw },
+        content: { id: id as Id, pw },
         auth
     })
 }
 
 const Login = () => {
-    const [id, setId] = useState<Id>('23-000')
+    const [id, setId] = useState<string>('')
     const [pw, setPw] = useState<string>('')
 
     return (
         <div css={css`
             display: flex;
             flex-direction: column;
+            width: 500px;
+            border-radius: 10px;
+            border: 2px solid black;
+            padding: 20px;
+            gap: 20px;
         `}>
-            <p>로그인</p>
-            <label>
-                ID:
+            <div css={css`
+                display: flex;
+                justify-content: center;
+            `}>로그인</div>
+            <div css={css`
+                display: grid;
+                grid-template-columns: 1fr 9fr;
+                grid-template-rows: 1fr 1fr;
+                gap: 10px;
+            `}>
+
+                <div css={css`
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    grid-column: 1 / 2;
+                    grid-row: 1 / 2;
+                `}>
+                    ID:
+                </div>
                 <input
+                    css={css`
+                        grid-column: 2 / 3;
+                        grid-row: 1 / 2;
+                        height: 30px;
+                    `}
                     defaultValue=''
                     onChange={
-                        (e) => {
-                            const value = e.target.value as Id
-                            setId(value)
-                        }
+                        (e) => setId(e.target.value || '')
                     }
                 />
-            </label>
-            <label>
-                PW:
+                <div css={css`
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    grid-column: 1 / 2;
+                    grid-row: 2 / 3;
+                `}>
+                    PW:
+                </div>
                 <input
+                    css={css`
+                        grid-column: 2 / 3;
+                        grid-row: 2 / 3;
+                        height: 30px;
+                    `}
                     type='password'
                     defaultValue=''
                     onChange={
-                        (e) => {
-                            const value = e.target.value || ''
-                            setPw(value)
-                        }
+                        (e) => setPw(e.target.value || '')
                     }
                 />
-            </label>
-            <button onClick={() => requestLogin(id, pw)}>Login</button>
+            </div>
+            <button
+                css={css`
+                    height: 40px;
+                    font-size: 25px;
+                `}
+                onClick={() => requestLogin(id, pw)}>Login</button>
         </div>
     )
 }
