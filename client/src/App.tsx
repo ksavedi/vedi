0e99ed751ef538a0f650b28aa6de9f0a82526970
bennyk0406 <kbenny0406@gmail.com>
2023-05-04 20:20:30 +0900

    modify frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,10 @@
 import Login from './Login';
+import Project from './Project';
 
 const App = () =>
-    <Login />
+    <>
+        <Login />
+        <Project />
+    </>
 
 export default App;
--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,5 +1,7 @@
 import type { Auth, ClientMsg } from '../../interface'
-const socket = new WebSocket('ws://localhost:3000')
+import { ServerMsg } from "../../interface"
+
+const socket = new WebSocket('wss://localhost:3000')
 
 const auth: Auth = {
     token: window.localStorage['token'],
@@ -11,4 +13,22 @@ function send(clientMsg: ClientMsg) {
     socket.send(JSON.stringify(clientMsg))
 }
 
-export { auth, send }
+socket.onmessage = (event: MessageEvent<string>) => {
+    const serverMsg = JSON.parse(event.data) as ServerMsg
+    const { query, content } = serverMsg
+    if (query === "error") {
+        window.alert(content.message)
+        return
+    }
+    if (query === "token") {
+        return
+    }
+    if (query === "projectList") {
+        return
+    }
+    if (query === "project") {
+        return
+    }
+}
+
+export { socket, auth, send }
