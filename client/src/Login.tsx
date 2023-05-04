4d191cc9673a9c7ed9405b3eeeb9f07c770b7a31
bennyk0406 <kbenny0406@gmail.com>
2023-05-04 15:17:30 +0900

    A little change

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -1,5 +1,3 @@
-/** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react'
 import { useState } from 'react'
 import { auth, send } from './socket'
 import { Id } from '../../interface'
