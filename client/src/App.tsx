5a4f8fdf5de21fe22f02813b4f888f60a7c87997
bennyk0406 <kbenny0406@gmail.com>
2023-05-05 23:43:03 +0900

    use browserRouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,13 @@
 import Login from './Login';
-import { auth, bindQuery, send, socket } from './socket';
-import { useEffect, useState } from 'react';
-import { ServerMsgAuthorized } from '../../interface/serverMsg';
 import ViewProject from './ViewProject';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const App = () => {
-    const [authorized, setAuthorized] = useState<boolean>(false);
-
-    useEffect(() => {
-        bindQuery.authorized = (content: ServerMsgAuthorized['content']) => {
-            setAuthorized(content.authorized)
-        }
-    })
-
-    socket.addEventListener('open', () => {
-        send({
-            query: 'getAuthorized',
-            content: null,
-            auth
-        })
-    })
-
-    if (authorized) return <ViewProject />
-    return <Login />
-}
+const App = () =>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/project" element={<ViewProject />} />
+        </Routes>
+    </BrowserRouter>
 
 export default App;
