3b95c6f32cf62a9e55a5be00da9c18a59e844236
bennyk0406 <kbenny0406@gmail.com>
2023-05-05 11:05:44 +0900

    a little change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,23 @@
 import Login from './Login';
 import ViewProject from './ViewProject';
 import { auth, send } from './socket';
+import './App.css'
 
 const App = () => {
     return (
         <>
-            <button onClick={() => send({
-                query: 'getProjectList',
-                content: null,
-                auth
-            })}>
-                getProjectList
-            </button>
+            <div id="nevigation-bar">
+                <button
+                    id="project-list-button"
+                    onClick={() => send({
+                        query: 'getProjectList',
+                        content: null,
+                        auth
+                    })}
+                >
+                    Test
+                </button>
+            </div>
             <Login />
             <ViewProject />
         </>
--- interface/msg.ts
+++ interface/msg.ts
@@ -1,4 +1,4 @@
-type Batch = 19 | 20 | 21 | 22 | 23 | 24
+type Batch = 19 | 20 | 21 | 22 | 23 | 24 | 25
 type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 type Id = null | `${Batch}-${0 | 1 | 2}${Digit}${Digit}`
 
