9fdc350dfad397257c0b4444f6c23f8fb4ca4b77
Junee <juneekim7@gmail.com>
2023-05-06 22:09:35 +0900

    cookie

--- server.ts
+++ server.ts
@@ -1,10 +1,13 @@
 //서버 열기
 import { resolve } from 'path'
 import express from 'express'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 app.use(express.static(resolve(__dirname, 'client/build')))
 app.use(express.json())
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cookieParser())
 app.get('*', (req, res) => {
     res.sendFile(resolve(__dirname, 'client/build/index.html'));
 })
@@ -36,32 +39,36 @@ const isAuthorized = (sessionKey: string) => {
     return sessionKey in session
 }
 
-const login = (auth: User): ServerMsg => {
-    if (checkValid(auth)) {
-        const sessionKey = generateSessionKey()
-        session[sessionKey] = auth.id
-        return {
-            query: 'loginResult',
-            content: {
-                result: true,
-                sessionKey
-            }
-        }
-    }
-    else {
-        return {
-            query: 'error',
-            content: {
-                message: '아이디나 비밀번호가 틀립니다.'
-            }
-        }
-    }
-}
-
 app.post('/api', (req, res) => {
     const send = (serverMsg: ServerMsg) => {
         res.json(serverMsg)
     }
+    
+    const login = (auth: User): ServerMsg => {
+        if (checkValid(auth)) {
+            const sessionKey = generateSessionKey()
+            session[sessionKey] = auth.id
+            res.cookie('sessionKey', sessionKey, {
+                maxAge: 60 * 60 * 1000
+            })
+            return {
+                query: 'loginResult',
+                content: {
+                    result: true,
+                    sessionKey
+                }
+            }
+        }
+        else {
+            return {
+                query: 'error',
+                content: {
+                    message: '아이디나 비밀번호가 틀립니다.'
+                }
+            }
+        }
+    }
+
 
     const clientMsg = req.body as ClientMsg
     const { query, content, sessionKey } = clientMsg
