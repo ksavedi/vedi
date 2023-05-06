8e807c9766660ac82476d86b5590c235f9a1fe16
Junee <juneekim7@gmail.com>
2023-05-06 20:30:30 +0900

    api

--- client/src/post.ts
+++ client/src/post.ts
@@ -1,35 +1,11 @@
 import type { ClientMsg } from '../../interface/clientMsg'
-import type { ServerMsg, ServerMsgAlert, ServerMsgError, ServerMsgLoginResult, ServerMsgProject, ServerMsgProjectList } from '../../interface/serverMsg'
-
-const socket = new WebSocket('ws://localhost:3000')
 
 function send(clientMsg: ClientMsg) {
-    socket.send(JSON.stringify(clientMsg))
+    fetch('./api', {
+        method: 'POST',
+        body: JSON.stringify(clientMsg)
+    }).then((res) => res.json())
+    .then((result) => { console.log(result) })
 }
 
-type BindQuery = {
-    [T in Exclude<ServerMsg, ServerMsgError | ServerMsgAlert> as T['query']]: (content: T['content']) => void;
-}
-
-const bindQuery: BindQuery = {
-    loginResult: (content: ServerMsgLoginResult["content"]) => { },
-    projectList: (content: ServerMsgProjectList["content"]) => { },
-    project: (content: ServerMsgProject["content"]) => { }
-}
-
-socket.onmessage = (event: MessageEvent<string>) => {
-    const serverMsg = JSON.parse(event.data) as ServerMsg
-    const { query, content } = serverMsg
-    if (query === "error") {
-        window.alert(`Error: ${content.message}`)
-        return
-    }
-    if (query === 'alert') {
-        window.alert(content.message)
-        return
-    }
-    // @ts-ignore
-    bindQuery[query](content)
-}
-
-export { socket, send, bindQuery }
+export { send }
--- server.ts
+++ server.ts
@@ -57,7 +57,7 @@ const login = (auth: User): ServerMsg => {
     }
 }
 
-app.post('/msg', (req, res) => {
+app.post('/api', (req, res) => {
     const send = (serverMsg: ServerMsg) => {
         res.json(serverMsg)
         return true
